825b3ca1401c517744e74becdd936f66cfdca94a
William Woodhead <wwoodhead@cloudflare.com>
2022-04-28 10:39:40 +0100

    added headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'set-cookie',
+            value: 'hello=there',
+          },
+        ],
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
