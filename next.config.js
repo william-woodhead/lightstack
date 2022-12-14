ba12b09851aa71e58a753b9262b3c628995b4605
William Woodhead <wwoodhead@cloudflare.com>
2022-12-14 10:10:19 +0000

    remove cookie and cache control

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/',
-        headers: [
-          {
-            key: 'set-cookie',
-            value: 'hello=there',
-          },
-          {
-            key: 'Link',
-            value: '<https://www.lightstack.co.uk/manifest.txt>; rel="prefetch"',
-          },
-        ],
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
