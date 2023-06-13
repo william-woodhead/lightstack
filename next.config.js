ff068227e99b9e9f1f6af4b37f705383a9262604
William Woodhead <wwoodhead@cloudflare.com>
2023-06-13 17:04:14 +0100

    added tester to manifest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [{
+      source: "/",
+      headers:[
+        {
+          key: "Link",
+          value: '<https://www.lightstack.co.uk/manifest.txt>; rel="prefetch"'
+        }
+      ]
+  }]
+  }
 }
 
 module.exports = nextConfig
