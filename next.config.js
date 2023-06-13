0630f629cb17df51a3ab7b556faf74182ff1c620
William Woodhead <wwoodhead@cloudflare.com>
2023-06-13 17:27:14 +0100

    added max age 120 to tester page

--- next.config.js
+++ next.config.js
@@ -8,9 +8,17 @@ const nextConfig = {
         {
           key: "Link",
           value: '<https://www.lightstack.co.uk/manifest.txt>; rel="prefetch"'
-        }
+        },
       ]
-  }]
+    }, {
+      source: "/tester",
+      headers:[
+        {
+          key: "Cache-Control",
+          value: "max-age=120",
+        },
+      ]
+    }]
   }
 }
 
