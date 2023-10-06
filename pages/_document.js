29b5f96f6cdf54c0c79c6f9e2adaedb5bf71f599
William Woodhead <wwoodhead@cloudflare.com>
2023-10-06 14:27:51 +0100

    moved links to body

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
+      </Head>
+      <body>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
-      </Head>
-      <body>
         <Main />
         <NextScript />
       </body>
