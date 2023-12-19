6b1aefd7c441a30d18f3ad0dc756b8271be34c41
William Woodhead <wwoodhead@cloudflare.com>
2023-12-19 16:22:43 +0000

    more fonts testing

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,7 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
-        {/* <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" /> */}
-        <link href="https://fonts.googleapis.com/css?family=Sen:regular,700,300,600|Averia+Serif+Libre:regular,700,300|Zilla+Slab:regular|Lora:regular&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
