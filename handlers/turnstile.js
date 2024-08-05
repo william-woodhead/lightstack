831c163e94b1d1de4b9a4ab383b6cf66c058e35d
William Woodhead <wwoodhead@cloudflare.com>
2024-08-05 14:16:49 +0100

    remove logs

--- handlers/turnstile.js
+++ handlers/turnstile.js
@@ -3,12 +3,9 @@ const path = require('path');
 const SECRET_KEY = process.env.LIGHTSTACK_TURNSTILE_SECRET_KEY;
 
 module.exports = async (req, res) => {
-  console.log("here we are", req.body);
   // Turnstile injects a token in "cf-turnstile-response".
 	const token = req.body['cf-turnstile-response'];
 	const ip = req.header('CF-Connecting-IP');
-  console.log("token", token);
-  console.log("ip", ip);
 	// Validate the token by calling the
 	// "/siteverify" API endpoint.
 	const url = 'https://challenges.cloudflare.com/turnstile/v0/siteverify';
